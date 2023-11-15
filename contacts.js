1488088b1be3aaa9e6ddd865c87099d96243f09d
Tkachuk-Yaroslav <tkachuky105@gmail.com>
2023-11-15 16:26:41 +0200

    second step part 1
    add listContacts() logic

---
+++ contacts.js
@@ -0,0 +1,33 @@
+const fs = require("fs/promises");
+const path = require("path");
+
+const contactsPath = path.join(__dirname, "db", "contacts.json");
+// console.log(contactsPath);
+
+// TODO: задокументувати кожну функцію
+async function listContacts() {
+  const data = await fs.readFile(contactsPath, "utf-8");
+  //   console.log(JSON.parse(data));
+  return JSON.parse(data);
+
+  // ...твій код. Повертає масив контактів.
+}
+
+function getContactById(contactId) {
+  // ...твій код. Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
+}
+
+function removeContact(contactId) {
+  // ...твій код. Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
+}
+
+function addContact(name, email, phone) {
+  // ...твій код. Повертає об'єкт доданого контакту.
+}
+
+module.exports = {
+  listContacts,
+  //   getContactById,
+  //   removeContact,
+  //   addContact,
+};
--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-console.log("Hello World!");
-console.log("first");
+// console.log("Hello World!");
+// console.log("first");
+
+const contacts = require("./contacts");
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "read":
+      const allContacts = await contacts.listContacts();
+      return console.log(allContacts);
+
+    default:
+      return console.log("Uncknown action!");
+  }
+};
+
+invokeAction({ action: "read" });
